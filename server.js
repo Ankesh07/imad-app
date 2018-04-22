90d86a3ec82b7e49158049e243b497d35707bba1
Ankesh Kumar <34163729+Ankesh07@users.noreply.github.com>
2018-04-22 23:23:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,97 +5,88 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-
 var articles={
-    'articleOne':{
-     title:'Article One | Atul Gawai',
-     heading:'Article One',
-     date:'Feb 13, 2018',
-     content:` <p>
-                This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.
-            </p>
-             <p>
-                This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.
-            </p>
-             <p>
-                This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.
-            </p>
-            `
-            
-    
- },
- 'articleTwo': { title:'Article Two | Atul Gawai',
-     heading:'Article Two',
-     date:'Feb 14, 2018',
-     content:` <p>
-                This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.
-            </p>
-             
-        `},
- 'articleThree':{ title:'Article Three | Atul Gawai',
-     heading:'Article Three',
-     date:'Feb 15, 2018',
-     content:` <p>
-                This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for my article.This is the content for  my article.This is the content for my article.
-            </p>
-             
-        `}
+    'article-one':{
+    title:'Article One!Ankesh Kumar',
+    heading:'Article One',
+    date:'3 April,2018',
+    content: ` <p>
+                      This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.
+                  </p>
+                  
+                   <p>
+                      This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.
+                  </p>
+                  <p>
+                      This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.
+                  </p>`
+},
+    'article-two':{
+    title:'Article Two!Ankesh Kumar',
+    heading:'Article Two',
+    date:'6 April,2018',
+    content: `<div><p>This is an HTML page</p><p>This is an HTML page</p><p>This is an HTML page</p><p>This is an HTML page</p>
+                <p>This is an HTML page</p><p>This is an HTML page</p><p>This is an HTML page</p><p>This is an HTML page</p>
+                <p>This is an HTML page</p><p>This is an HTML page</p><p>This is an HTML page</p></div>`},
+    'article-three':{
+    title:'Article Three!Ankesh Kumar',
+    heading:'Article Three',
+    date:'24 Sep,2018',
+    content: `<div><p>This is an HTML page</p><p>This is an HTML page</p><p>This is an HTML page</p><p>This is an HTML page</p>
+                <p>This is an HTML page</p><p>This is an HTML page</p><p>This is an HTML page</p><p>This is an HTML page</p>
+                <p>This is an HTML page</p><p>This is an HTML page</p><p>This is an HTML page</p></div>`}
 };
 function createTemplate(data){
- var title= data.title;
- var date = data.date;
- var content = data.content;
- var heading = data.heading;
- var htmlTemplate = `
- <html>
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+var htmlTemplate=`<html>
     <head>
-        <title> ${title}
-        
+        <title>
+            ${title}
         </title>
-        <meta name="viewport" content="width=device-width",initial-scale=1/>
-    <link href="/ui/style.css" rel="stylesheet" />
+        <meta name="viewport" content="width-device-scale, initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
     </head>
-    <body>
-
-<div class="container">        <div>
-            <a href="/">Home</a>
-        </div>
-        <hr/>
-        <h3>
-         ${heading}   
-        </h3>
-        <div>
-            ${date}
-        </div>
-        <div>
-          ${content}
-            </div>
-</div>
-    </body>
+ <body>
+     <div class="container">
+             <div>
+                 <a href="/">Home</a>
+             </div>
+             <hr/>
+             <h1>${heading}</h1>
+             <div>
+                ${date}
+             </div>
+              <div>
+                 ${content}
+              </div>
+    </div>
+ </body>
     
 </html>`;
-return htmlTemplate;}
-
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
-app.get('/:articleName', function(req,res){
-    //articleName== article- one
-    //article[articleName]=={} content object for article one
-    var articleName= req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
-});
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/:articleName',function(req,res){
+    var articleName=req.params.articleName;
+   res.send(createTemplate(articles[articleName]));
+});
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80

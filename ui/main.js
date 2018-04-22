a394b30964381d3d212a6ddeeb753c84d24f83ac
Ankesh Kumar <34163729+Ankesh07@users.noreply.github.com>
2018-04-22 23:50:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,7 @@
 console.log('Loaded!');
-
+var element=document.getElementById('main');
+var ele=document.getElementById('main');
+element.onclick=function(){
+    element.innerHTML='Rit@nku'
+}
+element.innerHTML=ele.innerHTML;

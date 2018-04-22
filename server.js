e6d4c17e3330de92bcbaf42f48e72b32d8ef429e
Ankesh Kumar <34163729+Ankesh07@users.noreply.github.com>
2018-04-22 21:28:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,69 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles={
+    articleOne:{
+    title:'Article One!Ankesh Kumar',
+    heading:'Article One',
+    date:'3 April,2018',
+    content: ` <p>
+                      This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.
+                  </p>
+                  
+                   <p>
+                      This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.
+                  </p>
+                  <p>
+                      This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.This is my first HTML page.
+                  </p>`
+},
+    articleTwo:{
+    title:'Article Two!Ankesh Kumar',
+    heading:'Article Two',
+    date:'6 April,2018',
+    content: `<p>This is an HTML page</p><p>This is an HTML page</p><p>This is an HTML page</p><p>This is an HTML page</p>
+                <p>This is an HTML page</p><p>This is an HTML page</p><p>This is an HTML page</p><p>This is an HTML page</p>
+                <p>This is an HTML page</p><p>This is an HTML page</p><p>This is an HTML page</p>`},
+    articleThree:{
+    title:'Article Three!Ankesh Kumar',
+    heading:'Article Three',
+    date:'24 Sep,2018',
+    content: `<p>This is an HTML page</p><p>This is an HTML page</p><p>This is an HTML page</p><p>This is an HTML page</p>
+                <p>This is an HTML page</p><p>This is an HTML page</p><p>This is an HTML page</p><p>This is an HTML page</p>
+                <p>This is an HTML page</p><p>This is an HTML page</p><p>This is an HTML page</p>`}
+};
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+var htmlTemplate=`<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width-device-scale, initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+ <body>
+     <div class="container">
+             <div>
+                 <a href="/">Home</a>
+             </div>
+             <hr/>
+             <h1>${heading}</h1>
+             <div>
+                ${date}
+             </div>
+              <div>
+                 ${content}
+              </div>
+    </div>
+ </body>
+    
+</html>`;
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -17,15 +80,11 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/article-one',function(req,res){
-   res.sendFile(path.join(__dirname,'ui','article-one.html'));
+app.get('/:articleName',function(req,res){
+    var articleName=req.params.articleName;
+   res.send(createTemplate(articles[articleName]));
 });
-app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','article-two.html'));
-});
-app.get('/article-three',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','article-three.html'));
-});
+
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 

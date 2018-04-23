134d6e049d3f47f80bd24d325522597875fe1913
Ankesh Kumar <34163729+Ankesh07@users.noreply.github.com>
2018-04-23 07:48:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,9 @@
 
 var counter=document.getElementById('counter');
-var count=document.getElementById('count');
+
 counter=0;
 counter.onclick=function(){
+    var count=document.getElementById('count');
     counter=counter+1;
     count.innerHTML=counter.toString();
 };

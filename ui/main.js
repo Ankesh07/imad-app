fcbb82fdcb8364096e3dda2a684b51a4fe351f4b
Ankesh Kumar <34163729+Ankesh07@users.noreply.github.com>
2018-04-23 07:47:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,7 @@
-console.log('Loaded!');
-var element=document.getElementById('main');
-var ele=document.getElementById('main');
-element.onclick=function(){
-    element.innerHTML='Rit@nku';
-};
-
-var img=document.getElementById('img');
-   var marginLeft=0;
-function moveRight(){
-  marginLeft=marginLeft+3;
- img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-  var interval=setInterval(moveRight,50);  
-};
 
 var counter=document.getElementById('counter');
 var count=document.getElementById('count');
 count=0;
 counter.onclick=function(){
-    count.innerHTML=count+1;
-}
+    count.innerHTML=count.toString();
+};
